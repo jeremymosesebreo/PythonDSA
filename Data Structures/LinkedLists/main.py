d08605580b041f7ae8733394a94b3e9111f23769
JM Ebreo <theg3rmmm@gmail.com>
2024-07-08 16:46:47 +0800

    Added Insertion Sort and Bubble Sort Implementations and fixed some minor code

--- Data Structures/LinkedLists/main.py
+++ Data Structures/LinkedLists/main.py
@@ -73,8 +73,70 @@ P.add_last(7)
 #Print the elements from the PositionalList
 for x in P:
     print(x)
-print("Delete the last element from the list")
-P.delete(P.last())
-#Print the elements from the PositionalList
+
+# Define the bubble sort function
+def bubble_sort(L):
+    n = len(L)
+    for i in range(n):
+        swapped = False
+        current = L.first()
+        for j in range(n - i - 1):
+            next = L.after(current)
+            if current.element() > next.element():
+                a = next.element()
+                b = current.element()
+                L.replace(current, a)
+                L.replace(next, b)
+                swapped = True
+            current = next
+        if not swapped:
+            break
+
+# Sort the PositionalList
+#bubble_sort(P)
+#print("The sorted list of elements are: ")
+# Print the sorted elements
+#for x in P:
+#    print(x)
+
+def insertion_sort(L):
+    '''Sort the Positional List of comparable elements into non decreasing order.'''
+    if len(L) > 1: #otherwise, no need to sort it
+        marker = L.first()
+        while marker != L.last():
+            pivot = L.after(marker)#next item to place
+            value = pivot.element()
+            if value > marker.element():#pivot is already sorted
+                marker = pivot#pivot becomes new marker
+            else:#must relocate pivot
+                walk = marker#find the leftmost value greater than pivot
+                while walk != L.first() and L.before(walk).element() > value:
+                    walk = L.before(walk)
+                L.delete(pivot)#remove pivot
+                L.add_before(walk, value)#insert pivot
+insertion_sort(P)
+print("The sorted list of elements are: ")
+# Print the sorted elements
+for x in P:
+    print(x)
+#change the insertion sort to descending order
+def insertion_sort_descending(L):
+    '''Sort the Positional List of comparable elements into non decreasing order.'''
+    if len(L) > 1: #otherwise, no need to sort it
+        marker = L.first()
+        while marker != L.last():
+            pivot = L.after(marker)#next item to place
+            value = pivot.element()
+            if value < marker.element():#pivot is already sorted
+                marker = pivot#pivot becomes new marker
+            else:#must relocate pivot
+                walk = marker#find the leftmost value greater than pivot
+                while walk != L.first() and L.before(walk).element() < value:
+                    walk = L.before(walk)
+                L.delete(pivot)#remove pivot
+                L.add_before(walk, value)#insert pivot
+insertion_sort_descending(P)
+print("The sorted list of elements are: ")
+# Print the sorted elements
 for x in P:
     print(x)
